e90b6c26bc6ad6d961b02b25f12689562bd39b10
Pierre Abbat <phma@bezitopo.org>
2025-07-31 07:57:59 +0000

    Compute a row of stepp and the nonlinearity of a vector

---
+++ src/DaphneCryptanalysis.jl
@@ -0,0 +1,55 @@
+module DaphneCryptanalysis
+using DaphneCipher,OffsetArrays
+import DaphneCipher:stepp
+import OffsetArrays:Origin
+export stepRow,nonlinearity
+
+function hadamard(buf::OffsetVector{<:Real})
+  tmp0=copy(buf)
+  tmp1=copy(buf)
+  sz=length(buf)
+  h=sz÷2
+  if ispow2(sz) && Origin(buf)==Origin((0,))
+    while h>0
+      for i in 0:sz-1
+	j=i⊻h
+	if i>j
+	  @inbounds tmp1[i]=tmp0[j]-tmp0[i]
+	else
+	  @inbounds tmp1[i]=tmp0[j]+tmp0[i]
+	end
+      end
+      tmp0,tmp1=tmp1,tmp0
+      if h%3==1
+	tmp0./=2
+      end
+      h÷=2
+    end
+  end
+  if length(buf)%3==2
+    tmp0.*=√2
+  end
+  tmp0
+end
+
+function stepRow(left::UInt8,right::UInt8)
+  row=OffsetVector(UInt8[],-1)
+  for i in 0x00:0xff
+    push!(row,stepp(i,left,right))
+  end
+  row
+end
+
+function nonlinearity(bytes::OffsetVector{UInt8})
+  buf=OffsetVector(Float64[],-1)
+  for b in bytes
+    for i in 0:7
+      push!(buf,((b>>i)&1)*-2+1)
+    end
+  end
+  had=hadamard(buf)
+  maxNonlin=√(8*length(had))
+  (maxNonlin-maximum(abs.(had)))/maxNonlin
+end
+
+end # module DaphneCryptanalysis
